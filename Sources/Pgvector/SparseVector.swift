bfaab9de0b4accf2e1e19bba1c56d7c7e5ce8621
Andrew Kane <andrew@ankane.org>
2025-02-16 22:58:51 -0800

    Improved SparseVector [skip ci]

--- Sources/Pgvector/SparseVector.swift
+++ Sources/Pgvector/SparseVector.swift
@@ -19,12 +19,44 @@ public struct SparseVector: Equatable {
     }
 
     public init?(_ string: String) {
-        return nil
+        let parts = string.split(separator: "/", maxSplits: 3)
+        if parts.count != 2 || parts[0].first != "{" || parts[0].last != "}" {
+            return nil
+        }
+
+        let elements = parts[0]
+        let dim = Int(parts[1])
+        if dim == nil {
+            return nil
+        }
+        var indices: [Int] = []
+        var values: [Float] = []
+
+        if (elements.count > 2) {
+            let start = elements.index(elements.startIndex, offsetBy: 1)
+            let end = elements.index(elements.endIndex, offsetBy: -1)
+            for e in elements[start..<end].split(separator: ",") {
+                let ep = e.split(separator: ":", maxSplits: 3)
+                if ep.count != 2 {
+                    return nil
+                }
+                if let i = Int(ep[0]), let v = Float(ep[1]) {
+                    indices.append(i - 1)
+                    values.append(v)
+                } else {
+                    return nil
+                }
+            }
+        }
+
+        self.dim = dim!
+        self.indices = indices
+        self.values = values
     }
 
     public func text() -> String {
-        let elements = zip(indices, values).map { String($0 + 1) + ":" + String($1) }.joined(separator: ",")
-        return "{" + elements + "}/" + String(dim)
+        let elements = zip(indices, values).map { String($0 + 1) + ":" + String($1) }
+        return "{" + elements.joined(separator: ",") + "}/" + String(dim)
     }
 
     public static func == (lhs: SparseVector, rhs: SparseVector) -> Bool {
