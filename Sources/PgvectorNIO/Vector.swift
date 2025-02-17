976ea8c8430ba90c80af27663bd2601897969196
Andrew Kane <andrew@ankane.org>
2025-02-17 00:25:17 -0800

    Added Vector encoding for PostgresNIO [skip ci]

--- Sources/PgvectorNIO/Vector.swift
+++ Sources/PgvectorNIO/Vector.swift
@@ -1,2 +1,23 @@
 import Pgvector
 import PostgresNIO
+
+extension Vector: @retroactive PostgresDynamicTypeEncodable {
+    public var psqlType: PostgresDataType {
+        PostgresDataType(223516)
+    }
+
+    public var psqlFormat: PostgresFormat {
+        .binary
+    }
+
+    public func encode<JSONEncoder: PostgresJSONEncoder>(
+        into byteBuffer: inout ByteBuffer,
+        context: PostgresEncodingContext<JSONEncoder>
+    ) {
+        byteBuffer.writeInteger(Int16(value.count), as: Int16.self)
+        byteBuffer.writeInteger(0, as: Int16.self)
+        for v in value {
+            byteBuffer.writeInteger(v.bitPattern)
+        }
+    }
+}
